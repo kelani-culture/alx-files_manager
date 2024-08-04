4044ef556668b08699db8b47743b034dce3b86fa
Null <kelanidarasimi9@gmail.com>
2024-08-04 17:22:34 +0100

    task 7

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -119,6 +119,67 @@ class FilesController {
 
     return res.status(200).json(files);
   }
+
+  static async putPublish(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const { id } = req.params;
+
+    if (!ObjectId.isValid(id)) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    const fileCollection = dbClient.db.collection('files');
+    const file = await fileCollection.findOne({ _id: new ObjectId(id), userId });
+
+    if (!file) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    await fileCollection.updateOne({ _id: new ObjectId(id) }, { $set: { isPublic: true } });
+    const updatedFile = await fileCollection.findOne({ _id: new ObjectId(id), userId });
+
+    return res.status(200).json(updatedFile);
+  }
+
+  static async putUnpublish(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const { id } = req.params;
+
+    if (!ObjectId.isValid(id)) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    const fileCollection = dbClient.db.collection('files');
+    const file = await fileCollection.findOne({ _id: new ObjectId(id), userId });
+
+    if (!file) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    await fileCollection.updateOne({ _id: new ObjectId(id) }, { $set: { isPublic: false } });
+    const updatedFile = await fileCollection.findOne({ _id: new ObjectId(id), userId });
+
+    return res.status(200).json(updatedFile);
+  }
+
 }
 
 export default FilesController;
