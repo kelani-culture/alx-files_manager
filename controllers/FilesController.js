f8399c999d1146372323ec16cb5aa2de0f10116c
Null <kelanidarasimi9@gmail.com>
2024-08-04 17:25:59 +0100

    task 8

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -180,6 +180,42 @@ class FilesController {
     return res.status(200).json(updatedFile);
   }
 
+   static async getFile(req, res) {
+    const { id } = req.params;
+    const token = req.headers['x-token'];
+
+    if (!ObjectId.isValid(id)) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    const fileCollection = dbClient.db.collection('files');
+    const file = await fileCollection.findOne({ _id: new ObjectId(id) });
+
+    if (!file) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    if (!file.isPublic) {
+      const userId = await redisClient.get(`auth_${token}`);
+      if (!userId || userId !== file.userId.toString()) {
+        return res.status(404).json({ error: 'Not found' });
+      }
+    }
+
+    if (file.type === 'folder') {
+      return res.status(400).json({ error: "A folder doesn't have content" });
+    }
+
+    try {
+      const fileContent = await fs.readFile(file.localPath);
+      const mimeType = mime.lookup(file.name) || 'application/octet-stream';
+      res.setHeader('Content-Type', mimeType);
+      return res.send(fileContent);
+    } catch (error) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+  }
+
 }
 
 export default FilesController;
