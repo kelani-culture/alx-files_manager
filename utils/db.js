001e8f5be8a48b8389aad16318b32d40291a169e
Null <kelanidarasimi9@gmail.com>
2024-08-04 16:50:30 +0100

    task 1

---
+++ utils/db.js
@@ -0,0 +1,39 @@
+import { MongoClient } from 'mongodb';
+import { promisify } from 'util';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+
+    const url = `mongodb://${host}:${port}`;
+    this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    this.client.connect((err) => {
+      if (err) {
+        console.error('MongoDB client not connected to the server:', err);
+      } else {
+        console.log('MongoDB client connected to the server');
+        this.db = this.client.db(database);
+      }
+    });
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const collection = this.db.collection('users');
+    return collection.countDocuments();
+  }
+
+  async nbFiles() {
+    const collection = this.db.collection('files');
+    return collection.countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
