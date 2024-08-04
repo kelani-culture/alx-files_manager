5273c2e860312b70c98af14cb34945d0ce349e91
Null <kelanidarasimi9@gmail.com>
2024-08-04 17:14:52 +0100

    task 5

---
+++ controllers/FilesController.js
@@ -0,0 +1,76 @@
+import { v4 as uuidv4 } from 'uuid';
+import { promises as fs } from 'fs';
+import path from 'path';
+import dbClient from '../utils/db.js';
+import redisClient from '../utils/redis.js';
+
+class FilesController {
+  static async postUpload(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const { name, type, parentId = 0, isPublic = false, data } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ error: 'Missing name' });
+    }
+
+    if (!['folder', 'file', 'image'].includes(type)) {
+      return res.status(400).json({ error: 'Missing type' });
+    }
+
+    if (type !== 'folder' && !data) {
+      return res.status(400).json({ error: 'Missing data' });
+    }
+
+    const fileCollection = dbClient.db.collection('files');
+    if (parentId !== 0) {
+      const parentFile = await fileCollection.findOne({ _id: new ObjectId(parentId) });
+
+      if (!parentFile) {
+        return res.status(400).json({ error: 'Parent not found' });
+      }
+
+      if (parentFile.type !== 'folder') {
+        return res.status(400).json({ error: 'Parent is not a folder' });
+      }
+    }
+
+    const newFile = {
+      userId,
+      name,
+      type,
+      isPublic,
+      parentId,
+    };
+
+    if (type === 'folder') {
+      await fileCollection.insertOne(newFile);
+      return res.status(201).json(newFile);
+    }
+
+    const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
+    const localPath = path.join(folderPath, uuidv4());
+
+    try {
+      await fs.mkdir(folderPath, { recursive: true });
+      await fs.writeFile(localPath, Buffer.from(data, 'base64'));
+    } catch (error) {
+      return res.status(500).json({ error: 'Cannot store the file' });
+    }
+
+    newFile.localPath = localPath;
+    await fileCollection.insertOne(newFile);
+
+    return res.status(201).json(newFile);
+  }
+}
+
+export default FilesController;
